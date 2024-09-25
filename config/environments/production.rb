b7d51e7ef6ec966cb8a55b656c6d2cbf38e1dd50
nakmai <carbani1253@gmail.com>
2024-09-25 22:18:59 +0900

    config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
+  config.logger.formatter = config.log_formatter
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false

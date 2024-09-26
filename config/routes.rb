20fe2f58ef8c6e544c9b2434b29c05758c4c7618
nakmai <carbani1253@gmail.com>
2024-09-27 00:58:40 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,16 @@ Rails.application.routes.draw do
     # ログアウト完了ページ
   get 'logout_complete', to: 'home#logout_complete'
 
+    # all_form ページへのルートを追加
+  get 'users/all_form', to: 'users#all_form'
+
+   # 規約ページへのルート
+  get 'terms', to: 'pages#terms'
+
   # ルートページ ("/")
   root 'home#index'
 
-  # 規約ページへのルート
-  get 'terms', to: 'pages#terms'
-  
+
 end
 
 

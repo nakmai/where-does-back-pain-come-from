863e73a21b6fa6be16eb5bc5d47fb7088691fcb5
nakmai <carbani1253@gmail.com>
2024-09-27 08:48:38 +0900

    フォーム画面分岐OK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,17 @@ class PagesController < ApplicationController
     def terms
       # 規約ページを表示するためのアクション
     end
+
+    def gynecology
+      # 婦人科
+    end
+
+    def orthopedics
+      # 整形外科
+    end
+
+    def red_flag
+      # レッドフラッグ
+    end
+
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Devise のルーティング設定
   devise_for :users
+
+  # ユーザー関連ルート
+  resources :users, only: [:create] 
   
   # ホームページ
   get 'home/index'
@@ -8,20 +11,29 @@ Rails.application.routes.draw do
   # ログイン後のページ
   get 'after_sign_in', to: 'home#after_sign_in'
 
-    # ログアウト完了ページ
+  # ログアウト完了ページ
   get 'logout_complete', to: 'home#logout_complete'
 
-    # all_form ページへのルートを追加
+  # all_form ページへのルート
   get 'users/all_form', to: 'users#all_form'
+  post 'users/submit_form', to: 'users#submit_form'  # submit_form への POST リクエスト
 
-   # 規約ページへのルート
+  # 規約ページへのルート
   get 'terms', to: 'pages#terms'
+
+  # 整形外科受診ページへのルート
+  get 'orthopedics', to: 'pages#orthopedics'
+
+  # レッドフラッグページへのルート
+  get 'red_flag', to: 'pages#red_flag'
+
+  # 婦人科 ページへのルート
+  get 'gynecology', to: 'pages#gynecology'
 
   # ルートページ ("/")
   root 'home#index'
-
-
 end
+
 
 
 

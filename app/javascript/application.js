4a7acd73ef08509f5ca12f080a3c0dac693bc3c9
nakmai <carbani1253@gmail.com>
2024-09-30 17:20:49 +0900

    トルクボタン修正２

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // app/javascript/application.js
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import { Turbo } from "@hotwired/turbo-rails"
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"

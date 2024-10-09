53a6ec65f2ebc94eacc24178090eeeb518eb9486
nakmai <carbani1253@gmail.com>
2024-10-10 01:05:51 +0900

    config/environments・environments・applicationを編集

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Test1111
     # アプリケーションのデフォルト言語を日本語に設定
     config.i18n.default_locale = :ja
 
+    # 許可するホストを設定
+    config.hosts << "where-does-back-pain-come-from-07a8b4b21813.herokuapp.com"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -21,8 +24,8 @@ module Test1111
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-  end # <- This closes the class Application
-end   # <- This closes the module Test1111
+  end
+end
 
 
 

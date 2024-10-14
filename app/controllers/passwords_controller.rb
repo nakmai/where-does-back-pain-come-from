6f8f2bc50b1d2f9b035cd8fd9cb51ce8596aed9e
nakmai <carbani1253@gmail.com>
2024-10-14 12:10:58 +0900

    ”パスワード再登録時_フラッシュメッセージ設定3”

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,6 +8,7 @@ class PasswordsController < Devise::PasswordsController
     # 2. パスワードリセット用URLクリック時の処理
     # Deviseのeditアクションが呼び出され、パスワード変更フォームが表示される
     def edit
+      # resource_class.new で新しいリソースを作成し、リセットトークンをセット
       self.resource = resource_class.new
       resource.reset_password_token = params[:reset_password_token]
       super
@@ -16,32 +17,32 @@ class PasswordsController < Devise::PasswordsController
     # 3. 新しいパスワードを設定後の処理
     def update
       # パスワードの長さと構成の確認
-      if params[:user][:password].length < 6 || params[:user][:password].length > 20 ||
-        !(params[:user][:password] =~ /[A-Z]/ && params[:user][:password] =~ /[a-z]/ && params[:user][:password] =~ /\d/)
-       flash.now[:alert] = "6文字以上〜20文字以内で小文字・大文字・数字を組み合わせたパスワードを設定してください"
-       render :edit and return
-     end
-
-      # パスワード更新処理を実行
-      self.resource = resource_class.reset_password_by_token(resource_params)
-
-      # パスワードが不一致の場合
-    if params[:user][:password] != params[:user][:password_confirmation]
-      flash.now[:alert] = "パスワードが一致していません"
-      render :edit and return
-    end
-  
-      # パスワードが無事に変更されたかどうかを確認
-      if resource.errors.empty?
-        # ログインさせず、パスワード変更後にログインページへリダイレクトする
-        yield resource if block_given?
-        set_flash_message!(:notice, :updated_not_active) # Deviseが提供するメッセージを表示
-        redirect_to new_user_session_path # 4. 登録完了後にログインページにリダイレクト
-      else
-        # エラーがあれば再度パスワード変更ページを表示
-        respond_with resource
+      def update
+        # パスワードが不一致の場合
+        if params[:user][:password] != params[:user][:password_confirmation]
+          flash.now[:alert] = "パスワードが一致していません"
+          render :edit and return
+        end
+    
+        # パスワードの長さと構成の確認
+        if params[:user][:password].length < 6 || params[:user][:password].length > 20 ||
+           !(params[:user][:password] =~ /[A-Z]/ && params[:user][:password] =~ /[a-z]/ && params[:user][:password] =~ /\d/)
+          flash.now[:alert] = "6文字以上〜20文字以内で小文字・大文字・数字を組み合わせたパスワードを設定してください"
+          render :edit and return
+        end
+    
+        # パスワード更新処理を実行
+        self.resource = resource_class.reset_password_by_token(resource_params)
+    
+        # パスワードが無事に変更されたかどうかを確認
+        if resource.errors.empty?
+          yield resource if block_given?
+          set_flash_message!(:notice, :updated_not_active)
+          redirect_to new_user_session_path
+        else
+          respond_with resource
+        end
       end
-    end
   
     protected
   

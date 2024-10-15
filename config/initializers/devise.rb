a69735f91b7c9377ff48bb11b9c53e9516d9ffe3
nakmai <carbani1253@gmail.com>
2024-10-15 17:49:01 +0900

    ”googleアカウントでログイン可能”

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,7 +275,6 @@ Devise.setup do |config|
 
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
   OmniAuth.config.logger = Rails.logger if Rails.env.development?
-  
 
 
   # ==> Warden configuration

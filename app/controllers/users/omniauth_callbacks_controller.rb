1a56378537ae5ff04b623f382cabcab50eb822be
nakmai <carbani1253@gmail.com>
2024-10-15 21:39:50 +0900

    生年月日googleアカウントテスト用3

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,23 +2,35 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def google_oauth2
-      # Google認証情報を取得
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-  
-      if @user.persisted?
+  # Google OAuth2のコールバック処理
+  def google_oauth2
+    # Google認証情報を元にユーザーを検索または新規作成
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    # ユーザーがデータベースに存在し、保存されている場合
+    if @user.persisted?
+      if @user.birthday.present? && @user.gender.present?
+        # 生年月日と性別が既に登録されている場合は通常通りログイン後のページにリダイレクト
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       else
-        session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+        # 生年月日や性別が登録されていない場合はフォームページにリダイレクト
+        sign_in @user
+        redirect_to users_all_form_path
       end
-    end
-  
-    def failure
-      redirect_to root_path
+    else
+      # ユーザーが作成されなかった場合は、新規登録ページにリダイレクトし、エラーメッセージを表示
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
+
+  # 認証に失敗した場合の処理
+  def failure
+    redirect_to root_path
+  end
+end
+
   
   
   

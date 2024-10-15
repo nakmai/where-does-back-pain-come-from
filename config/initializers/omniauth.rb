cbd603063716a053b4105d59f98759cab70b7c76
nakmai <carbani1253@gmail.com>
2024-10-15 14:52:01 +0900

    ”googleログイン_omniauth5”

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
       scope: 'email,profile',
-      prompt: 'select_account'
+      prompt: 'select_account',
+      provider_ignores_state: false # CSRF保護を有効にする
     }
   end
   
   OmniAuth.config.allowed_request_methods = [:post]
+  
   

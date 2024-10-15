f7cb3f3e5d04aa25db5fa1f26382b78441883b32
nakmai <carbani1253@gmail.com>
2024-10-15 14:01:19 +0900

    ”googleログイン_device変更”

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -268,12 +268,18 @@ Devise.setup do |config|
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
 
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
-
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+
+  Devise.setup do |config|
+    config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+      scope: 'email,profile',
+      prompt: 'select_account',
+      skip_jwt: true
+    }
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

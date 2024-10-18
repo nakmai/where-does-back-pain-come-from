0d5111a8678ce7f6df54a50bcf6497cb5acea63a
nakmai <carbani1253@gmail.com>
2024-10-18 20:15:26 +0900

    ”googleログイン退会後再度登録_データベース作成”

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,34 +1,35 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
   def google_oauth2
+    # Google OAuth 2.0 認証データを取得
     auth = request.env['omniauth.auth']
-  
-    # Googleからのデータを取得してセッションに保存
+
+    # Googleからの必要データを抽出
     google_data = extract_google_data(auth)
     session[:google_data] = google_data
-  
+
+    # ユーザーをOmniAuthのデータを基に検索または新規作成
     @user = User.from_omniauth(auth)
-  
+
     if @user.persisted?
-      # 退会済みのアカウントであれば、再登録の処理を追加
-      if @user.deleted_at.present?
-        @user.update(deleted_at: nil)
-      end
-  
-      # 生年月日や性別がない場合でもトップページにリダイレクトして後で入力させる
+      # 退会済みのアカウントが存在する場合、`deleted_at`をリセット
+      reactivate_user_if_deleted(@user)
+
+      # ユーザーが正常に保存されていればサインインし、リダイレクト
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       flash[:notice] = "登録が完了しました。生年月日や性別を後で入力してください。"
+
+      # 年齢・性別に基づくリダイレクト
+      age = calculate_age(google_data[:birthdate])
+      redirect_based_on_age_and_gender(age, google_data[:gender])
     else
-      # ユーザーが保存されない場合
+      # ユーザーが保存されない場合、エラーメッセージとともに登録ページにリダイレクト
       session['devise.google_data'] = auth.except(:extra)
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
-  
-  
 
   private
 
@@ -38,16 +39,25 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       gender: auth.extra&.raw_info&.gender || auth.info&.gender
     }
   end
-  
+
+  def reactivate_user_if_deleted(user)
+    if user.deleted_at.present?
+      user.update(deleted_at: nil)
+    end
+  end
 
   def calculate_age(birthdate)
+    return nil unless birthdate
     ((Time.zone.now - birthdate.to_time) / 1.year.seconds).floor
   end
 
   def redirect_based_on_age_and_gender(age, gender)
-    if age <= 20 || age >= 55
+    return redirect_to root_path, alert: "無効な年齢データです。" unless age
+
+    case age
+    when 0..20, 55..150
       redirect_to orthopedics_advice1_path
-    elsif age >= 21 && age <= 54
+    when 21..54
       if gender == "male"
         redirect_to red_flag_path
       elsif gender == "female"
@@ -60,3 +70,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 end
+

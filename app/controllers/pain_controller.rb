17aab0e9a1dbaf63b4531a2e213f3ebf360f0de1
nakmai <carbani1253@gmail.com>
2024-11-23 13:53:40 +0900

    app/controllers/pain_controller.rb修正

--- app/controllers/pain_controller.rb
+++ app/controllers/pain_controller.rb
@@ -49,9 +49,7 @@ class PainController < ApplicationController
       render :red_flag2 # red_flag2ページを再表示
       return
     end
-  end
 
- 
     # 「特に問題なし」と他の項目が両方選ばれている場合
     if conditions.include?('no_issues') && conditions.length > 1
       flash.now[:alert] = '無効な選択です'
@@ -76,7 +74,7 @@ class PainController < ApplicationController
   def red_flag3_submit
     flash.clear
     conditions = params[:conditions] || []
-  
+
     # チェックボックスが選択されていない場合
     if conditions.empty?
       flash.now[:alert] = '選択してください'
@@ -84,7 +82,7 @@ class PainController < ApplicationController
       render :red_flag3 # 再表示
       return
     end
-  
+
     # 特定の条件が選ばれた場合、orthopedics_advice5_pathにリダイレクト
     pain_conditions = ['severe_pain', 'no_rest_pain', 'pain_at_rest', 'pain_at_norest', 'wake_from_pain', 'sleep_disorder']
     
@@ -93,29 +91,28 @@ class PainController < ApplicationController
       redirect_to orthopedics_advice5_path
       return
     end
-  
+
     # 「特になし」が選ばれている場合のリダイレクト
     if conditions.include?('no_issues')
       redirect_to pain_location_extention_flexion_path
       return
     end
-  
+
     # 「特になし」と no_pain や no_pain2 の条件が選ばれている場合にリダイレクト
     if (conditions.include?('no_issues') && (conditions.include?('no_pain') || conditions.include?('no_pain2')))
       redirect_to pain_location_extention_flexion_path
       return
     end
-  
+
     # それ以外の条件でのリダイレクト
     if (conditions.include?('no_pain') || conditions.include?('no_pain2'))
       redirect_to pain_location_extention_flexion_path
       return
     end
-  
+
     # 最後に、どの条件にも該当しない場合は、再度 red_flag3 を表示
     render :red_flag3
   end
-  
 
   def pain_scale
     # 何もしない。ビューを表示するだけ。

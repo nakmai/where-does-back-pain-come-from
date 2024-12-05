03b66ff7aa41eccab09eed34c0c58013e9f36da6
nakmai <carbani1253@gmail.com>
2024-12-05 18:56:45 +0900

    ”OGP設定”

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def show_meta_tags
-        assign_meta_tags if display_meta_tags.blank?
+        set_meta_tags(default_meta_tags) if display_meta_tags.blank?
         display_meta_tags
       end
     
@@ -16,8 +16,15 @@ module ApplicationHelper
           icon: [                    #favicon、apple用アイコンを指定する
             { href: image_url('スライド1.png') },
             { href: image_url('top_page.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
-          ]
+          ],
+          og: {                              # Open Graph Protocolの設定
+          title: :あなたの腰痛どこから,
+          description: 'このアプリは腰痛を簡易的にタイプ別に分け、セルフケア方法をお伝えするアプリです。',
+          image: image_url('top_page.png'),
+          url: request.original_url,
+          type: 'website'
+        }        
         }
       end
-      end
+    end
 end

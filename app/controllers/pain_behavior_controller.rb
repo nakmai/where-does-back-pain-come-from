d51110c7fd9275c45304a2ed7c369325b73a7327
nakmai <carbani1253@gmail.com>
2024-12-12 13:08:41 +0900

    ”過剰ファイル削除”

--- app/controllers/pain_behavior_controller.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class PainBehaviorController < ApplicationController
-end
--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class UserMailer < ApplicationMailer
-  def welcome_email(user)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-  end
-end
